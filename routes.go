9ec2b75ef3fc7aca41849f87c6bdb8c1cb925263
Arc41t3ct <floridait@gmail.com>
2024-10-22 22:11:19 +0200

    removed everything from landing

---
+++ routes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	chi "github.com/go-chi/chi/v5"
+)
+
+func (a *application) routes() *chi.Mux {
+	// middleware must come before any routes using aliases
+	a.use(a.Middlware.Remember)
+	// routes go here using the aloases
+	a.get("/", a.Handlers.Home)
+	// static routes
+	fileServer := http.FileServer(http.Dir("./public"))
+	a.App.Routes.Handle("/public/*", http.StripPrefix("/public", fileServer))
+
+	return a.App.Routes
+}
